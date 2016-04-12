41c14aabbb3705e14106388ef73786718f64ff22
Rafael Alba <rafael.alba.lopez@gmail.com>
2016-04-11 17:53:23 -0700

    breaking code and added fixtures

--- django_waitlist/urls.py
+++ django_waitlist/urls.py
@@ -5,9 +5,6 @@ from django.views.generic import TemplateView
 
 from django.contrib import admin
 
-from . import views
-
-
 urlpatterns = [
     url(r"^$", TemplateView.as_view(template_name="homepage.html"), name="home"),
     url(r"^admin/", include(admin.site.urls)),
