ebc9eb92052b29c73bdf224acc9a77f634820bdc
Rafael Alba <rafael@tcell.io>
2016-08-19 16:58:21 -0700

    fake initial doesn't exist for 1.7

--- docker/initial_setup.sh
+++ docker/initial_setup.sh
@@ -4,7 +4,7 @@ sleep 4 # wait for the database to start up
 echo "CREATING django_waitlist_dev"
 docker-machine ip | xargs -I % psql -h % -p 5432 -d postgres -U postgres -c 'create database django_waitlist_dev'
 echo "Running Migrations"
-docker-compose run web python manage.py migrate --fake-initial
+docker-compose run web python manage.py migrate
 echo "Seeding DB"
 docker-compose run web python manage.py loaddata waitlist_entries
 docker-compose stop
