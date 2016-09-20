0dd540736cfba1d2dddeae6077a42a586ba3b39a
Rafael <rafael@tcell.io>
2016-09-20 09:05:22 -0700

    no more create_ab

--- waitlist_entries/urls.py
+++ waitlist_entries/urls.py
@@ -5,5 +5,4 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^create/$', views.create, name='create'),
-    url(r'^create_ab/$', views.create_ab, name='create_ab'),
 ]
