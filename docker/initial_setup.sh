c437eef11b677ee2ecac04a9b457101acf703039
Rafael <rafael@tcell.io>
2017-02-24 23:48:19 -0800

    rely on DOCKER_IP being set in the env

--- docker/initial_setup.sh
+++ docker/initial_setup.sh
@@ -1,8 +1,10 @@
+: ${DOCKER_IP:?"needs to be set in your env (ex: export DOCKER_IP=0.0.0.0)"}
+
 docker-compose up -d postgreshost
 echo "WAITING FOR DATABASE TO START"
 sleep 4 # wait for the database to start up
 echo "CREATING django_waitlist_dev"
-dinghy ip | xargs -I % psql -h % -p 5432 -d postgres -U postgres -c 'create database django_waitlist_dev'
+psql -h $DOCKER_IP -p 5432 -d postgres -U postgres -c 'create database django_waitlist_dev'
 echo "Running Migrations"
 docker-compose run web python manage.py migrate
 echo "Seeding DB"
