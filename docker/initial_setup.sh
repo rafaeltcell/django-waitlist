c489e9cf2cc97eb56ce892138d3d7cd0fbbef81a
Rafael <rafael@tcell.io>
2017-03-30 11:22:47 -0700

    add a separate db for django 1.7

--- docker/initial_setup.sh
+++ docker/initial_setup.sh
@@ -1,3 +1,11 @@
+echo "Creating django_waitlist_dev for Django 1.7"
+docker-compose run --rm init-postgres17
+echo "Running Migrations for Django 1.7"
+docker-compose run --rm web17 python manage.py migrate
+echo "Seeding DB for Django 1.7"
+docker-compose run --rm web17 python manage.py loaddata waitlist_entries accounts
+docker-compose stop
+
 echo "Creating django_waitlist_dev"
 docker-compose run --rm init-postgres
 echo "Running Migrations"
