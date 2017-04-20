6c6922340a944328ce205236ee0eed42c6b17692
Rafael Alba <rafael@tcell.io>
2017-04-20 12:08:44 -0700

    send django log to console

--- django_waitlist/settings.py
+++ django_waitlist/settings.py
@@ -140,28 +140,44 @@ INSTALLED_APPS = [
 # the site admins on every HTTP 500 error when DEBUG=False.
 # See http://docs.djangoproject.com/en/dev/topics/logging for
 # more details on how to customize your logging configuration.
+# LOGGING = {
+    # "version": 1,
+    # "disable_existing_loggers": False,
+    # "filters": {
+        # "require_debug_false": {
+            # "()": "django.utils.log.RequireDebugFalse"
+        # }
+    # },
+    # "handlers": {
+        # "mail_admins": {
+            # "level": "ERROR",
+            # "filters": ["require_debug_false"],
+            # "class": "django.utils.log.AdminEmailHandler"
+        # }
+    # },
+    # "loggers": {
+        # "django.request": {
+            # "handlers": ["mail_admins"],
+            # "level": "ERROR",
+            # "propagate": True,
+        # },
+    # }
+# }
+
 LOGGING = {
-    "version": 1,
-    "disable_existing_loggers": False,
-    "filters": {
-        "require_debug_false": {
-            "()": "django.utils.log.RequireDebugFalse"
-        }
-    },
-    "handlers": {
-        "mail_admins": {
-            "level": "ERROR",
-            "filters": ["require_debug_false"],
-            "class": "django.utils.log.AdminEmailHandler"
-        }
-    },
-    "loggers": {
-        "django.request": {
-            "handlers": ["mail_admins"],
-            "level": "ERROR",
-            "propagate": True,
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
         },
-    }
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'INFO'),
+        },
+    },
 }
 
 FIXTURE_DIRS = [
