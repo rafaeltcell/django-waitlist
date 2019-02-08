31dd62b20b713b464c8ddaf1278f723dacc4f42c
Rafael Alba <rafael.alba.lopez@gmail.com>
2019-02-08 10:21:57 -0800

    add health startup check to app

--- django_waitlist/wsgi.py
+++ django_waitlist/wsgi.py
@@ -10,6 +10,7 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from django_waitlist.settings import PROJECT_ROOT
 
 # import tcell_agent
 # tcell_agent.init()
@@ -17,10 +18,24 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_waitlist.settings")
 
 application = get_wsgi_application()
-application.load_middleware()
 
-# import newrelic.agent
-# application = newrelic.agent.WSGIApplicationWrapper(application)
-
-# os.environ['TCELL_AGENT_CONFIG'] = '/app/tcell_agent.config'
-# os.environ['TCELL_AGENT_HOME'] = '/app/tcell'
+# try:
+#     # Capture revision of running uwsgi
+#     # Assumes you run chdir with uwsgi
+#     if os.getenv('DOCKER_DEV'):
+#         bhash = 'hash'
+#     else:
+#         from subprocess import check_output
+#         bhash = check_output(['git','rev-parse','HEAD'])
+#     rev = open(os.path.join( PROJECT_ROOT, 'revision.txt') ,'wb')
+#     rev.write(bhash)
+#     rev.close()
+# 
+#     # Drop file for healthy check
+#     text = "This file exists only when uwsgi is healthy. Remove it to pool from LB pool"
+#     health = open( os.path.join( PROJECT_ROOT, 'health.check' ) ,'wb')
+#     health.write(text)
+#     health.close()
+# except:
+#     import traceback
+#     print traceback.format_exc()
